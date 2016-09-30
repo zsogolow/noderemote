8f6fedfed66f72212a0fe31d3c9a62e89f60ac8d
Zachery.Sogolow <zachsogolow@Msn.com>
2016-09-30 10:18:35 -0700

    ads

--- runner/runner.cpp
+++ runner/runner.cpp
@@ -10,6 +10,9 @@
 #include <inttypes.h>
 #include <string>
 #include <noderemote.h>
+#include <thread>
+#include <sys/socket.h>
+#include <sys/un.h>
 
 using namespace std;
 
@@ -137,6 +140,72 @@ void loop()
     listenForPackets();
 }
 
+void socketServer()
+{
+    struct sockaddr_un addr;
+    char buf[100];
+    int fd, cl, rc;
+
+    if (argc > 1)
+        socket_path = argv[1];
+
+    if ((fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1)
+    {
+        perror("socket error");
+        exit(-1);
+    }
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    if (*socket_path == '\0')
+    {
+        *addr.sun_path = '\0';
+        strncpy(addr.sun_path + 1, socket_path + 1, sizeof(addr.sun_path) - 2);
+    }
+    else
+    {
+        strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path) - 1);
+        unlink(socket_path);
+    }
+
+    if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) == -1)
+    {
+        perror("bind error");
+        exit(-1);
+    }
+
+    if (listen(fd, 5) == -1)
+    {
+        perror("listen error");
+        exit(-1);
+    }
+
+    while (1)
+    {
+        if ((cl = accept(fd, NULL, NULL)) == -1)
+        {
+            perror("accept error");
+            continue;
+        }
+
+        while ((rc = read(cl, buf, sizeof(buf))) > 0)
+        {
+            printf("read %u bytes: %.*s\n", rc, rc, buf);
+            write(fd, buf, rc);
+        }
+        if (rc == -1)
+        {
+            perror("read");
+            exit(-1);
+        }
+        else if (rc == 0)
+        {
+            printf("EOF\n");
+            close(cl);
+        }
+    }
+}
+
 int main(int argc, char **argv)
 {
     int dflag = 0;
@@ -208,11 +277,15 @@ int main(int argc, char **argv)
     }
     else if (tvalue == HEARTBEAT)
     {
+        thread t1(task1, "Hello");
+
         while (true)
         {
             loop();
         }
 
+        t1.join();
+        
         return 0;
     }
 
