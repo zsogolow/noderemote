a938054eb56150a866823352363a3152ac77bca9
Zachery.Sogolow <zachsogolow@Msn.com>
2016-09-30 10:22:05 -0700

    asd

--- runner/runner.cpp
+++ runner/runner.cpp
@@ -275,7 +275,7 @@ int main(int argc, char **argv)
     }
     else if (tvalue == HEARTBEAT)
     {
-        thread t1(task1, "Hello");
+        thread t1(socketServer);
 
         while (true)
         {
