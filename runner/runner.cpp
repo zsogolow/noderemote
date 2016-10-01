e5954893f7171fb3bdef3e47b25185e4f4f51c80
zsogolow <zachsogolow@msn.com>
2016-10-01 16:42:58 -0700

    ..

--- runner/runner.cpp
+++ runner/runner.cpp
@@ -102,7 +102,7 @@ Packet listenForPackets()
         //If we received the message in time, let's read it and print it
         radio.read(&heard, sizeof(heard));
         printf("wtf %u", heard.extra);
-        fprintf(stderr, "Yay! Got action %u from: 0x%" PRIx64 " (%u) with extra: %u.\n\r", heard.action, pipes[heard.id], heard.id, heard.extra);
+        printf("Yay! Got action %u from: 0x%" PRIx64 " (%u) with extra: %u.\n\r", heard.action, pipes[heard.id], heard.id, heard.extra);
         return heard;
     }
 }
