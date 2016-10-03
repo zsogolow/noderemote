c354b067e41fecf87f984a5083a29249e4f5df5c
Zachery.Sogolow <zachsogolow@Msn.com>
2016-10-03 07:46:33 -0700

    idk anymore

--- runner/runner.cpp
+++ runner/runner.cpp
@@ -65,13 +65,13 @@ bool listenForACK(int action)
         radio.read(&ack, sizeof(ack));
         if (ack.action == action)
         {
-            printf("%d", ack.extra);
+            printf("%u", ack.extra);
             fprintf(stderr, "Yay! Got action %u from: 0x%" PRIx64 " (%u) with extra: %u.\n\r", ack.action, pipes[ack.id], ack.id, ack.extra);
             return true;
         }
         else
         {
-            printf("%d", 1);
+            printf("%u", 1);
             return false;
         }
     }
