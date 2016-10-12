dc9e98b41b758de08e73f89fb6e3fab7059b4638
zsogolow <zachsogolow@msn.com>
2016-10-12 11:39:59 -0700

    a

--- sockets/socketClient.js
+++ sockets/socketClient.js
@@ -5,14 +5,13 @@ var socketPath = '/tmp/hidden';
 const client = net.connect(socketPath, () => {
     //'connect' listener
     console.log('connected to server!');
-    client.write('world!\r\n');
+    client.write('11');
 });
 
 client.on('data', (data) => {
     console.log(data.toString());
 
     // end after we get a response
-    client.end();
 });
 
 client.on('end', () => {
