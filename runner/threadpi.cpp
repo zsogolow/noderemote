76fa408b71e343e60793d818bfac53d2c8ca3e00
zsogolow <zachsogolow@msn.com>
2016-10-12 11:57:10 -0700

    dsds

--- runner/threadpi.cpp
+++ runner/threadpi.cpp
@@ -140,8 +140,8 @@ void handleSocketMessage(int rc, char buf[])
 
     prepareSocket();
 
-    int dvalue = atoi(buf[0]);
-    int tvalue = atoi(buf[1]);
+    int dvalue = atoi(&buf[0]);
+    int tvalue = atoi(&buf[1]);
 
     char *cvalue = "";
 
