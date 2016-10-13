2bb27adaa3caac315d559bfa90925c56171617b3
Zachery.Sogolow <zachsogolow@Msn.com>
2016-10-13 14:56:53 -0700

    sadf

--- runner/threadpi.cpp
+++ runner/threadpi.cpp
@@ -163,8 +163,8 @@ void handleSocketMessage(int rc, char buf[])
         if (success == false)
         {
             // needed for when we get no response from duino
-            buf[0] = dvalue; // id
-            buf[1] = tvalue; // action
+            buf[0] = itoa(dvalue); // id
+            buf[1] = itoa(tvalue); // action
             buf[2] = 0;      // type
             buf[3] = 0;      // extra
             write(fd, buf, 4);
